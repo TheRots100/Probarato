ab0caedf5b72999e0cd2cfc19167c8d5662a9afd
Aldair Torrez <torrezaldair777@gmail.com>
2022-10-16 13:28:32 -0400

    funcion de registro de negocio funcional

--- javascript/registronegocio.js
+++ javascript/registronegocio.js
@@ -1,21 +1,26 @@
 
-// fetch('https://jsonplaceholder.typicode.com/todos', {
-//   method: 'POST',
-//   body: JSON.stringify({
-    // id_Usuario : 1,
-    // nombre : getElementById("NombreEmpresa"),
-    // descrip : getElementById("Descripcion del Negocio"), 
-    // ubicacion : getElementById("DireccionNego"),
-    // telefono : getElementById("NumeroNego"),
-    // horarioInicio : getElementById("HoraIni"),
-    // horarioCierre : getElementById("HoraFin")
-//   }),
-//   headers: {
-//     "Content-type": "application/json"
-//   }
-// ).then(response => response.json())
-// .then(json => console.log(json))
+async function doPostRequest(){
+  fetch('http://localhost:8000/api/registro/negocio', {
+    method: 'POST',
+    body: JSON.stringify({
+      id_usuario : 1,
+      nombre : document.getElementById("NombreEmpresa").value,
+      descrip : document.getElementById("DescripcionNego").value, 
+      ubicacion : document.getElementById("DireccionNego").value,
+      telefono : document.getElementById("NumeroNego").value,
+      horario_inicio : document.getElementById("HoraIni").value,
+      horario_cierre : document.getElementById("HoraFin").value
+    }),
+    headers: {
+      "Content-type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  
+}
 
+document.getElementById('BotonReg').addEventListener('click', doPostRequest);
 // axios({
 //   url: 'http://tudireccion.com',
 //   method: 'post',
@@ -33,26 +38,24 @@
 // }).then(OkCallback, ErrorCallback)
 
 // var fs = require('fs');
-const axios = require('axios');
+// const axios = require('axios');
 
-async function doPostRequest() {
+// async function doPostRequest() {
 
-  let payload = 
-  {  
-    id_Usuario : 1,
-    nombre : getElementById("NombreEmpresa"),
-    descrip : getElementById("Descripcion del Negocio"), 
-    ubicacion : getElementById("DireccionNego"),
-    telefono : getElementById("NumeroNego"),
-    horarioInicio : getElementById("HoraIni"),
-    horarioCierre : getElementById("HoraFin")
-  };
+//   let payload = 
+//   {  
+//     id_Usuario : 1,
+//     nombre : getElementById("NombreEmpresa"),
+//     descrip : getElementById("Descripcion del Negocio"), 
+//     ubicacion : getElementById("DireccionNego"),
+//     telefono : getElementById("NumeroNego"),
+//     horarioInicio : getElementById("HoraIni"),
+//     horarioCierre : getElementById("HoraFin")
+//   };
 
-  let res = await axios.post('http://localhost:8000/api/registro/negocio', payload);
+//   let res = await axios.post('http://localhost:8000/api/registro/negocio', payload);
 
-  let data = res.data;
-  console.log(data);
-}
+//   let data = res.data;
+//   console.log(data);
+// }
 
-
-document.getElementById('BotonReg').addEventListener('click', doPostRequest);
