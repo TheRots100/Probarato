58be7ca6450cd4efa4363bec813fa2f3f04e831b
Perzival5 <www.str_hpl@hotmail.com>
2022-10-17 10:42:47 -0400

    se agrego verificacion

--- javascript/registronegocio.js
+++ javascript/registronegocio.js
@@ -1,5 +1,67 @@
 
-async function doPostRequest(){
+
+document.getElementById('formulario').addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let nombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+  let direccion = /^[a-zA-Z0-9\_\-]{4,16}$/;
+  let telefono = /^\d{7,14}$/;
+  nombreEmpresa = document.getElementById("NombreEmpresa").value;
+  if(nombreEmpresa== null || nombreEmpresa.length == 0 || nombre.test(nombreEmpresa)== false){
+    document.getElementById('NombreEmpresa').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NombreEmpresa').style.boxShadow = "0 0 0";}
+
+  nombreDueno = document.getElementById("NombreDueno").value;
+  if(nombreDueno== null || nombreDueno.length == 0 || nombre.test(nombreDueno)== false){
+    document.getElementById('NombreDueno').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NombreDueno').style.boxShadow = "0 0 0";}
+
+  dirNego = document.getElementById("DireccionNego").value;
+  if(dirNego == null || dirNego == 0 || direccion.test(dirNego)== false){
+    document.getElementById('DireccionNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('DireccionNego').style.boxShadow = "0 0 0";}
+
+  desNego = document.getElementById("DescripcionNego").value;
+  if(desNego == null || desNego.length == 0 ){
+    document.getElementById('DescripcionNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('DescripcionNego').style.boxShadow = "0 0 0";}
+
+  numero = document.getElementById("NumeroNego").value;
+  if(numero == null || numero.length == 0 || telefono.test(numero)==false){
+    document.getElementById('NumeroNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NumeroNego').style.boxShadow = "0 0 0";
+  Redir();
+}
+
+})
+
+
+//async function doPostRequest(){
+//  fetch('http://localhost:8000/api/registro/negocio', {
+//    method: 'POST',
+//    body: JSON.stringify({
+//      id_usuario : 1,
+//      nombre : document.getElementById("NombreEmpresa").value,
+//      descrip : document.getElementById("DescripcionNego").value, 
+//      ubicacion : document.getElementById("DireccionNego").value,
+//      telefono : document.getElementById("NumeroNego").value,
+//      horario_inicio : document.getElementById("HoraIni").value,
+//      horario_cierre : document.getElementById("HoraFin").value
+//    }),
+//    headers: {
+//      "Content-type": "application/json"
+//    }
+//  })
+//  .then(response => response.json())
+//  .then(json => console.log(json))
+//  
+//}
+
+function Redir(){
   fetch('http://localhost:8000/api/registro/negocio', {
     method: 'POST',
     body: JSON.stringify({
@@ -17,9 +79,6 @@ async function doPostRequest(){
   })
   .then(response => response.json())
   .then(json => console.log(json))
-  
-}
-function Redir(){
   location.href = "index.html";
 }
 document.getElementById('BotonReg').addEventListener('click', doPostRequest);
