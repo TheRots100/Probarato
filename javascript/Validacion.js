206d7947e5d100f07d4623062e4ae1b7185c4e50
wilfredo Sanabria <wilfredo_olviiver@hotmail.com>
2022-10-17 22:25:18 -0400

    Reparando Errores

--- javascript/Validacion.js
+++ javascript/Validacion.js
@@ -1,52 +1,52 @@
 
 document.getElementById('formulario').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let nombre = /^[a-zA-ZÀ-ÿ\s]{3,32}$/;
-    let direccion = /^[a-zA-Z0-9\_\-]{10,64}$/;
-    let telefono = /^\d{7,8}$/;
-    nombreEmpresa = document.getElementById("NombreEmpresa").value;
-    if(nombreEmpresa== null || nombreEmpresa.length == 0 || nombre.test(nombreEmpresa)== false){
-      document.getElementById('NombreEmpresa').style.boxShadow = "0 0 5px red";
-      return false;
-    }else{document.getElementById('NombreEmpresa').style.boxShadow = "0 0 0";}
-  
-    nombreDueno = document.getElementById("NombreDueno").value;
-    if(nombreDueno== null || nombreDueno.length == 0 || nombre.test(nombreDueno)== false){
-      document.getElementById('NombreDueno').style.boxShadow = "0 0 5px red";
-      return false;
-    }else{document.getElementById('NombreDueno').style.boxShadow = "0 0 0";
+  e.preventDefault();
+  let nombre = /^[a-zA-ZÀ-ÿ\s]{3,32}$/;
+  let direccion = /^[a-zA-Z0-9\_\-]{10,64}$/;
+  let telefono = /^\d{7,8}$/;
+  nombreEmpresa = document.getElementById("NombreEmpresa").value;
+  if(nombreEmpresa== null || nombreEmpresa.length == 0 || nombre.test(nombreEmpresa)== false){
+    document.getElementById('NombreEmpresa').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NombreEmpresa').style.boxShadow = "0 0 0";}
+
+  nombreDueno = document.getElementById("NombreDueno").value;
+  if(nombreDueno== null || nombreDueno.length == 0 || nombre.test(nombreDueno)== false){
+    document.getElementById('NombreDueno').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NombreDueno').style.boxShadow = "0 0 0";
 }
-  
-    dirNego = document.getElementById("DireccionNego").value;
-    if(dirNego == null || dirNego == 0 || direccion.test(dirNego)== false){
-      document.getElementById('DireccionNego').style.boxShadow = "0 0 5px red";
-      return false;
-    }else{document.getElementById('DireccionNego').style.boxShadow = "0 0 0";}
-  
-    desNego = document.getElementById("DescripcionNego").value;
-    if(desNego == null || desNego.length == 0 ){
-      document.getElementById('DescripcionNego').style.boxShadow = "0 0 5px red";
-      return false;
-    }else{document.getElementById('DescripcionNego').style.boxShadow = "0 0 0";}
-  
-    numero = document.getElementById("NumeroNego").value;
-    if(numero == null || numero.length == 0 || telefono.test(numero)==false){
-      document.getElementById('NumeroNego').style.boxShadow = "0 0 5px red";
-      return false;
-    }else{document.getElementById('NumeroNego').style.boxShadow = "0 0 0";
-    Redir();
-  }
-  
-  })
-  function ValidarExt(){
 
-    var archivoInput = document.getElementById('LogoEmpresa');
-    var archivoRuta = archivoInput.value;
-    var extPermitida = /(.png|.jpeg|.jpg)$/i; 
+  dirNego = document.getElementById("DireccionNego").value;
+  if(dirNego == null || dirNego == 0 || direccion.test(dirNego)== false){
+    document.getElementById('DireccionNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('DireccionNego').style.boxShadow = "0 0 0";}
 
-    if(!extPermitida.exec(archivoRuta)){
-        document.getElementById('LogoEmpresa').style.boxShadow = "0 0 5px red";
-        return false;
-    }
-    else{document.getElementById('LogoEmpresa').style.boxShadow = "0 0 0";}
+  desNego = document.getElementById("DescripcionNego").value;
+  if(desNego == null || desNego.length == 0 ){
+    document.getElementById('DescripcionNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('DescripcionNego').style.boxShadow = "0 0 0";}
+
+  numero = document.getElementById("NumeroNego").value;
+  if(numero == null || numero.length == 0 || telefono.test(numero)==false){
+    document.getElementById('NumeroNego').style.boxShadow = "0 0 5px red";
+    return false;
+  }else{document.getElementById('NumeroNego').style.boxShadow = "0 0 0";
+  Redir();
+}
+
+})
+function ValidarExt(){
+
+  var archivoInput = document.getElementById('LogoEmpresa');
+  var archivoRuta = archivoInput.value;
+  var extPermitida = /(.png|.jpeg|.jpg)$/i; 
+
+  if(!extPermitida.exec(archivoRuta)){
+      document.getElementById('LogoEmpresa').style.boxShadow = "0 0 5px red";
+      return false;
   }
+  else{document.getElementById('LogoEmpresa').style.boxShadow = "0 0 0";}
+}
